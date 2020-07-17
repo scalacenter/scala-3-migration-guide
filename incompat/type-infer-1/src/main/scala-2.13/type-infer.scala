47ac0af9aba1eb4e6ce33b69bf472b8d0e309d87
adpi2 <adrien.piquerez@gmail.com>
2020-07-17 13:45:54 +0200

    add incompatibility typeInfer1

---
+++ incompat/type-infer-1/src/main/scala-2.13/type-infer.scala
@@ -0,0 +1,14 @@
+trait Context { type Out }
+
+object Context {
+  type Aux[A] = Context { type Out = A }
+  implicit val ctx: Aux[String] = ???
+}
+
+trait Foo[A]
+
+object Foo {
+  def foo(implicit ctx: Context): Foo[ctx.Out] = ???
+
+  def bar(f: Foo[String] => Int): Option[Int] = Option(foo).map(f) 
+}
