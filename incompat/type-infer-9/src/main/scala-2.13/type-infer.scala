65bddef6f193858ba059ffc46dfd5db6a4b4f0af
adpi2 <adrien.piquerez@gmail.com>
2020-07-24 12:57:25 +0200

    Add type-infer-9 incompatibility

---
+++ incompat/type-infer-9/src/main/scala-2.13/type-infer.scala
@@ -0,0 +1,17 @@
+trait IO[+E]
+
+trait Acquire[+E] {
+  def apply[E1 >: E](release: E1 => IO[Nothing]): Release[E]
+}
+
+trait Release[+E] {
+  def apply[E1 >: E](io: IO[E1]): IO[E1]
+}
+
+
+object IO {
+  def cleanup[E](e: E): IO[Nothing] = ???
+  def acquire[E]: Acquire[E] = ??? 
+
+  def foo(io: IO[Nothing]): IO[Nothing] = acquire(cleanup)(io)
+}
