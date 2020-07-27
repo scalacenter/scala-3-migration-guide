9fb1221dc8105b1b0a7bffb12e17646ca597cb90
Adrien Piquerez <adrien.piquerez@gmail.com>
2020-07-27 13:08:38 +0200

    Remove java-varargs incompatibility

--- incompat/java-varargs/src/main/java/test.java
+++
@@ -1,5 +0,0 @@
-class Test {
-  public static void test() {
-    Foo.foo(new String[]{ "a", "b" });
-  }
-}
--- incompat/java-varargs/src/main/scala/java-varargs.scala
+++
@@ -1,5 +0,0 @@
-object Foo {
-  def foo(values: Array[String]): Unit = foo(values.toSeq:_*)
-
-  def foo(values: String*): Unit = ???
-}
