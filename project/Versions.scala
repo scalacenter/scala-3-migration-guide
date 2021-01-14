5940f843977c1fa63bfaaef0096b25fb719d8370
Jakub Kozłowski <kubukoz@gmail.com>
2021-01-14 01:05:47 +0100

    Update versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,9 +1,9 @@
 object Versions {
   val scala213 = "2.13.4"
-  val scala30 = "3.0.0-M1"
+  val scala30 = "3.0.0-M3"
   val dotty = "0.27.0-RC1"
   val `scala-2.13.3` = "2.13.3"
-  val sbtDotty = "0.4.6"
+  val sbtDotty = "0.5.1"
   val osLib = "0.7.1"
   val sourcecode = "0.2.1"
 }
