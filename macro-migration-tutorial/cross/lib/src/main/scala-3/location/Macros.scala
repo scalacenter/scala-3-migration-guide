9289f093249ab1b3dab908c030e29221517ae8b3
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-02-15 15:08:45 +0100

    Fix macro migration build

--- macro-migration-tutorial/cross/lib/src/main/scala-3/location/Macros.scala
+++ macro-migration-tutorial/cross/lib/src/main/scala-3/location/Macros.scala
@@ -1,12 +1,13 @@
 package location
 
-import scala.quoted.{QuoteContext, Expr}
+import scala.quoted.{Quotes, Expr}
 
 object Macros:
   inline def location: Location = ${locationImpl}
 
-  private def locationImpl(using ctx: QuoteContext): Expr[Location] =
-    import ctx.reflect.rootPosition
-    val file = Expr(rootPosition.sourceFile.jpath.toString)
-    val line = Expr(rootPosition.startLine + 1)
+  private def locationImpl(using ctx: Quotes): Expr[Location] =
+    import ctx.reflect.Position
+    val pos = Position.ofMacroExpansion
+    val file = Expr(pos.sourceFile.jpath.toString)
+    val line = Expr(pos.startLine + 1)
     '{new Location($file, $line)}
--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,8 @@
 object Versions {
   val scala213 = "2.13.4"
   val scala30 = "3.0.0-M3"
+  // required for testing the Tasty reader
+  val scala3M1 = "3.0.0-M1"
   val sbtDotty = "0.4.7"
   val osLib = "0.7.2"
   val sourcecode = "0.2.3"
