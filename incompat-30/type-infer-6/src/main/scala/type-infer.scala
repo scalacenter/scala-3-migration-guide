7e4b4344e7e44c605fbd2fd2feb0fac69878654c
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-02-15 12:56:29 +0100

    Remove fixed incompatibilities

--- incompat-30/type-infer-6/src/main/scala/type-infer.scala
+++
@@ -1,7 +0,0 @@
-trait Show[A]
-
-object Test {
-  def foo[F[_], A](f: F[A]): F[A] = ???
-
-  def bar[A: Show, B: Show]: Show[A] = foo[Show, A](implicitly) 
-}
--- incompat-30/type-infer-8/src/main/scala-2.13/type-infer.scala
+++
@@ -1,9 +0,0 @@
-trait IO[+A, +E] {
-  def collectM[B, EE >: E](pf: PartialFunction[A, IO[B, EE]]): IO[B, EE]
-
-  def collect[B](pf: PartialFunction[A, B]): IO[B, E] = collectM(pf.andThen(IO.succeed _))
-}
-
-object IO {
-  def succeed[A](a: A): IO[A, Nothing] = ???
-}
