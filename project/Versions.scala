0e0b8571392a1d4ccb892e80894c480aebff47bf
Adrien Piquerez <adrien.piquerez@gmail.com>
2021-03-26 14:49:49 +0100

    bump sourcecode to 0.2.4

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,6 +4,6 @@ object Versions {
   // required for testing the Tasty reader
   val sbtDotty = "0.4.7"
   val osLib = "0.7.3"
-  val sourcecode = "0.2.3"
+  val sourcecode = "0.2.4"
   val scala3Migrate = "0.3.1"
 }
